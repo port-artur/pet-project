e85fab08fe9a78408eb6a4285e4365dec8a7f5a0
Alex <a>
2022-03-12 13:45:54 +0600

    Initial Commit

---
+++ Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by 🌚🌚 Царь🌚🌚 on 12.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
